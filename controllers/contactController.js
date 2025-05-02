dbc53074020b3e31353d1ceff759639b39c36f86
hassanNewton <hassan.hussin@newton.se>
2025-05-02 16:24:18 +0200

    simplecontroller for demo

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,85 +5,91 @@ const Contact = require("../models/contactModel");
 //@route GET /api/contacts
 //@access private
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find({user_id: req.user.id});
+  const contacts = await Contact.find({ user_id: req.user.id });
   res.status(200).json(contacts);
 });
 
-//@desc Create all contacts
+//@desc Create a new contact
 //@route POST /api/contacts
 //@access private
 const createContacts = asyncHandler(async (req, res) => {
-    console.log("The request body is", req.body);
-    const {name, email, phone} = req.body;
-    if(!name || !email || !phone){
-        res.status(400);
-        throw new Error("Please fill all the fields");
-    }
-    const contact = await Contact.create({
-        name,
-        email,
-        phone,
-        user_id: req.user.id,
-    });
+  console.log("The request body is", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  }
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+    user_id: req.user.id,
+  });
 
-    res.status(200).json(contact);
-    });
-    
+  res.status(200).json(contact);
+});
+
 //@desc GET all contacts
 //@route GET /api/contacts/:id
 //@access private
 const getContact = asyncHandler(async (req, res) => {
-    const contact = await Contact.findById(req.params.id);
-    if(!contact){
-        res.status(404);
-        throw new Error("Contact not found");
-    }
-    res.status(200).json(contact);
-    });
-    
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  res.status(200).json(contact);
+});
 
 //@desc Update all contacts
 //@route PUT /api/contacts/:id
 //@access private
 const updateContact = asyncHandler(async (req, res) => {
-    const contact = await Contact.findById(req.params.id);
-    if(!contact){
-        res.status(404);
-        throw new Error("Contact not found");
-    }
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
 
-    if(contact.user_id.toString() !== req.user.id){
-        res.status(403);
-        throw new Error("User not authorized");
-    }
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User not authorized");
+  }
 
-    const updatedContact = await Contact.findByIdAndUpdate(
-        req.params.id, 
-        req.body, 
-        {new: true});
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
 
-    res.status(200).json(updatedContact);
-    });
-    
+  res.status(200).json(updatedContact);
+});
 
 //@desc delete contact
 //@route DELETE /api/contacts/:id
 //@access private
 const deleteContact = asyncHandler(async (req, res) => {
-    const contact = await Contact.findById(req.params.id);
-    if(!contact){
-        res.status(404);
-        throw new Error("Contact not found");
-    }
-    
-    if(contact.user_id.toString() !== req.user.id){
-        res.status(403);
-        throw new Error("User not authorized");
-    }
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
 
-    // await Contact.remove()
-    await Contact.deleteOne({_id: req.params.id});;
-    res.status(200).json(contact);
-    });
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User not authorized");
+  }
 
-module.exports = {getContacts, createContacts, getContact, updateContact, deleteContact}; 
+  // await Contact.remove()
+  await Contact.deleteOne({ _id: req.params.id });
+  //   await contact.deleteOne(); tydligare med denna rad
+  res.status(200).json(contact);
+});
+
+module.exports = {
+  getContacts,
+  createContacts,
+  getContact,
+  updateContact,
+  deleteContact,
+};
